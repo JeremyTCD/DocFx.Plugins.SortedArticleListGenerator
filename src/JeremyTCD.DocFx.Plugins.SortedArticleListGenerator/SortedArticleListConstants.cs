b3007e87d422c7d77d36dca4de75b0a91a5fcfa6
JeremyTCD <jeremytanchongdao@gmail.com>
2018-01-09 18:45:27 +0800

    Replaced . with _ in variable names

--- src/JeremyTCD.DocFx.Plugins.SortedArticleListGenerator/SortedArticleListConstants.cs
+++ src/JeremyTCD.DocFx.Plugins.SortedArticleListGenerator/SortedArticleListConstants.cs
@@ -2,10 +2,10 @@
 {
     public static class SortedArticleListConstants
     {
-        public const string EnableSalKey = "jtcd.enableSal";
-        public const string SalSnippetLengthKey = "jtcd.salSnippetLength";
-        public const string IncludeInSalKey = "jtcd.includeInSal";
-        public const string DateKey = "jtcd.date";
+        public const string EnableSalKey = "jtcd_enableSal";
+        public const string SalSnippetLengthKey = "jtcd_salSnippetLength";
+        public const string IncludeInSalKey = "jtcd_includeInSal";
+        public const string DateKey = "jtcd_date";
         public const string SalAllItemsNodeId = "sal-all-items";
         public const string SalWrapperNodeId = "sorted-article-list";
         public const int DefaultSalSnippetLength = 500;
